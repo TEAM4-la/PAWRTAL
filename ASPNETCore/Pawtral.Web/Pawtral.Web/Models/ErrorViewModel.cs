5ca73e8c3c1f4f694a1de857bd01be96f6a7f067
Erwin Mendoza <khronosangel@gmail.com>
2026-03-07 20:56:38 +0800

    [Init] Base ASP.NET stared

---
+++ ASPNETCore/Pawtral.Web/Pawtral.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pawtral.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
